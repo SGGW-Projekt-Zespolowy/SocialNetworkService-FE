9367bbac2637ef7cdd875594b8f417913de42de9
Tomasz-Budny <s208246@sggw.edu.pl>
2023-04-04 17:01:31 +0200

    Add lazy loaded pages: login, registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) },
+  { path: 'registration', loadChildren: () => import('./auth/registration/registration.module').then(m => m.RegistrationModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/registration/registration.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './registration.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  {path: '', component: RegistrationComponent},
+];
+
+@NgModule({
+  declarations: [
+    RegistrationComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class RegistrationModule { }
