c946be444a554c716c911e2029e85b55a73470b3
Tomasz-Budny <s208246@sggw.edu.pl>
2023-04-04 18:52:22 +0200

    Add content to registration page

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
+  titles = ['Doktor', 'Student', 'rezydent'];
 
+  ngOnInit() {
+    this.registrationForm = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required
+      ]),
+      surname: new FormControl(null, [
+        Validators.required
+      ]),
+      password: new FormControl(null, 
+        [Validators.required,
+         Validators.minLength(7)
+        ]),
+      confirmPassword: new FormControl(null, 
+        [Validators.required, 
+         Validators.minLength(7),
+         // TODo: add custom validator to check if password is the same
+        ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      title: new FormControl(null, [
+        Validators.required
+      ])
+    });
+  }
+
+  onSubmitRegistrationForm() {
+    if(this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    }
+  }
 }
