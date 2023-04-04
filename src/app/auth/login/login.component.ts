48b3ab8b000a883696b093892ba9951c25d3bf20
Tomasz-Budny <s208246@sggw.edu.pl>
2023-04-04 18:25:00 +0200

    Add content to login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, Validators.email),
+      password: new FormControl(null),
+    });
+  }
+
+  onSubmitLoginForm() {
+    if(this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
