df44c5031145ca5795f921157d8e744bef4d3b0b
Tomasz-Budny <s208246@sggw.edu.pl>
2023-05-15 19:39:28 +0200

    Fix imports

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { PostComponent } from './post/post.component';
+import { PostComponent } from './posts-section/post/post.component';
 import { PostsSectionComponent } from './posts-section/posts-section.component';
 
 
