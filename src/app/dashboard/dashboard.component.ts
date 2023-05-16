9bf43e5d168bfe9ec5ff37ed97fe45a400f6ba20
Tomasz-Budny <s208246@sggw.edu.pl>
2023-05-16 22:48:30 +0200

    Use angular layout to hide sections

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  controlIsVisible: boolean = true;
+  friendsIsVisible: boolean = true;
+
+  controlDisapearBreakpoint: string = `(max-width: 1300px)`;
+  friendsDisapearBreakpoint: string = `(max-width: 900px)`;
+
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.responsive.observe([
+      this.controlDisapearBreakpoint
+      ])
+      .subscribe(result => {
+        this.controlIsVisible = true;
+        if (result.matches) {
+          this.controlIsVisible = false;
+        }
+    });
+
+    this.responsive.observe([
+      this.friendsDisapearBreakpoint
+      ])
+      .subscribe(result => {
+        this.friendsIsVisible = true;
+        if (result.matches) {
+          this.friendsIsVisible = false;
+        }
+    });
+  }
 }
