db05d90874b35173ea884b677089eb9aa0e9b1da
Tomasz-Budny <s208246@sggw.edu.pl>
2023-05-17 16:55:02 +0200

    Configure SvgRegistryService, Add test svg icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SvgIconRegistryService } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SocialNetworkService';
+export class AppComponent implements OnInit {
+  constructor(private iconReg: SvgIconRegistryService) { }
+
+  ngOnInit(): void {
+    this.iconReg.loadSvg('../assets/Icons/test-icon.svg', 'test').subscribe();
+  }
 }
