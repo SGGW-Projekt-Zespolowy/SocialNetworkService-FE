0aa601a17f0906e7d901abdb4a842fdc6736ebfa
Tomasz-Budny <s208246@sggw.edu.pl>
2023-05-18 20:42:17 +0200

    Add search bar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownInputComponent } from './dropdown-input/dropdown-input.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     DropdownInputComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchBarComponent
   ],
   imports: [
     CommonModule,
