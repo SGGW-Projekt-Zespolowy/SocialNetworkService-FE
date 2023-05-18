7de522d4e5021e0f750fa8502e2b24a998936110
Tomasz-Budny <s208246@sggw.edu.pl>
2023-05-18 21:25:37 +0200

    Move search-bar-input to distinct component

--- src/app/shared/search-bar-input/search-bar-input.component.ts
+++ src/app/shared/search-bar-input/search-bar-input.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar-input',
+  selector: 'search-bar-input',
   templateUrl: './search-bar-input.component.html',
   styleUrls: ['./search-bar-input.component.scss']
 })
