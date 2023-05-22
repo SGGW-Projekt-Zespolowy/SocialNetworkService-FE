4c333d1a2859cf2786a73ff7c877ba7a51ccdcaf
Tomasz-Budny <s208246@sggw.edu.pl>
2023-05-23 00:37:43 +0200

    Change app breakpoint

--- src/app/app-breakpoints.ts
+++ src/app/app-breakpoints.ts
@@ -1,5 +1,5 @@
 export class AppBreakpoints {
   static Large: string = `(max-width: 1300px)`;
-  static Medium: string = `(max-width: 900px)`;
+  static Medium: string = `(max-width: 950px)`;
   static Small: string = `(max-width: 500px)`
 }
