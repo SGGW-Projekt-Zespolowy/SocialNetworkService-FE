3f5f2d4c59625b6d27529aa811e4b9ffe6e609a4
Aleksander Lewicki <aleksanderlewy218@gmail.com>
2023-06-09 21:36:26 +0200

    Added home button and user profile navbar

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile.component';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    UserProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    UserProfileRoutingModule
-  ]
+    declarations: [
+        UserProfileComponent
+    ],
+    imports: [
+        CommonModule,
+        UserProfileRoutingModule,
+        SharedModule
+    ]
 })
 export class UserProfileModule { }
