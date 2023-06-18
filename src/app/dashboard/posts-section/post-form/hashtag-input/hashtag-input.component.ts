96aaf0371dba565a56dfffbc9b75b8ee7b9103e0
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-18 23:17:09 +0200

    .*

--- src/app/dashboard/posts-section/post-form/hashtag-input/hashtag-input.component.ts
+++ src/app/dashboard/posts-section/post-form/hashtag-input/hashtag-input.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { TagModel } from '../../post/post.component';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'hashtag-input',
   templateUrl: './hashtag-input.component.html',
-  styleUrls: ['./hashtag-input.component.scss']
+  styleUrls: ['./hashtag-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi:true,
+      useExisting: HashtagInputComponent
+    }
+  ]
 })
-export class HashtagInputComponent {
+export class HashtagInputComponent implements ControlValueAccessor {
   newHashtag: string = "";
   hashtags: TagModel[] = [];
 
@@ -16,9 +24,28 @@ export class HashtagInputComponent {
     this.newHashtag = "";
 
     this.hashtags.push({content: hashtagName, color: hashtagColor});
+    this.onChange(this.hashtags);
+    this.onTouched();
   }
 
   onDeleteHashtag(id: number) {
     this.hashtags.splice(id, 1);
+  }
+
+  onChange = (title) => {}
+  onTouched = () => {}
+  disabled: boolean = false;
+  
+  writeValue(hashtags: TagModel[]): void {
+    this.hashtags = hashtags;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 }
