85675c82f02c51a0685ab1da1896b760c3a2d730
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-18 23:16:56 +0200

    .*

--- src/app/dashboard/posts-section/post-form/post-form.component.ts
+++ src/app/dashboard/posts-section/post-form/post-form.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
   styleUrls: ['./post-form.component.scss']
 })
-export class PostFormComponent {
+export class PostFormComponent{
+
+  @Output() close = new EventEmitter<boolean>();
+
   titles = ['siema', 'jebany', 'chuju']
 
-  loginForm: FormGroup;
+  postForm = this.fb.group({
+    title: [null, [Validators.required]],
+    content: [null, [Validators.required]],
+    category: [null, [Validators.required]],
+    hashtags: [[], [Validators.required]]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
 
   onSubmit() {
-
+    if(this.postForm.valid) {
+      console.log(this.postForm.value)
+    }
   }
+
 }
