6a019ae2186f568323128031f5681fd12f73a8c9
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-26 20:32:57 +0200

    Rename comment model properties

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -17,33 +17,33 @@ export class CommentService {
   comments: CommentModel[] = [
     {
       id: '1',
-      opTitle: 'Student',
-      opName: 'Tomasz Budny',
+      authorDegree: 'Student',
+      authorName: 'Tomasz Budny',
       content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto consequatur nihil omnis eum molestias neque iste exercitationem sapiente? Ratione odio ut ab tenetur quis culpa, quo illo quasi sequi. Eveniet earum vero eligendi iusto repudiandae aspernatur dolore aliquam quo obcaecati.',
   
-      opAvatar: 'https://i.ytimg.com/vi/V8f-1olyC1s/maxresdefault.jpg',
+      authorAvatar: 'https://i.ytimg.com/vi/V8f-1olyC1s/maxresdefault.jpg',
       authorId: '1',
       isHelpful: false,
     },
   
     {
       id: '2',
-      opTitle: 'Student',
-      opName: 'Kowal Pawelczyk',
+      authorDegree: 'Student',
+      authorName: 'Kowal Pawelczyk',
       content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto consequatur nihil omnis eum molestias neque iste exercitationem sapiente? Ratione odio ut ab tenetur quis culpa, quo illo quasi sequi. Eveniet earum vero eligendi iusto repudiandae aspernatur dolore aliquam quo obcaecati dignissimos suscipit non exercitationem, beatae, est deserunt ab voluptas sequi! repudiandae aspernatur dolore aliquam quo obcaecati dignissimos suscipit non exercitationem, beatae, est deserunt ab voluptas sequi!',
   
-      opAvatar: 'https://img.freepik.com/free-photo/portrait-black-man-isolated_53876-40305.jpg',
+      authorAvatar: 'https://img.freepik.com/free-photo/portrait-black-man-isolated_53876-40305.jpg',
       authorId: '1',
       isHelpful: true,
     },
   
     {
       id: '3',
-      opTitle: 'Dr n. med.',
-      opName: 'janek Paweł Wypych',
+      authorDegree: 'Dr n. med.',
+      authorName: 'janek Paweł Wypych',
       content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto consequatur nihil omnis eum molestias neque iste exercitationem sapiente? Ratione odio ut ab tenetur quis culpa, quo illo quasi sequi. Eveniet earum vero eligendi iusto repudiandae aspernatur dolore aliquam quo obcaecati dignissimos suscipit non exercitationem, beatae, est deserunt ab voluptas sequi!',
   
-      opAvatar: 'https://thumbs.dreamstime.com/b/lekarz-medycyny-z-pastylkami-27402514.jpg',
+      authorAvatar: 'https://thumbs.dreamstime.com/b/lekarz-medycyny-z-pastylkami-27402514.jpg',
       authorId: '1',
       isHelpful: false,
     },
