334f684f22d475e1f19ff38406cd396c210e67ee
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-28 22:52:56 +0200

    Add imagePreviewModal and confirm modal to user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
 import { PostPopUpService } from '../services/post-pop-up.service';
+import { ConfirmModalService } from '../services/confirm-modal.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -12,6 +13,7 @@ export class UserProfileComponent {
   constructor(
     public popUpService: PostPopUpService,
     protected postService: PostService,
+    public confirmModalService: ConfirmModalService
   ) {}
 
   selectedTab: string;
