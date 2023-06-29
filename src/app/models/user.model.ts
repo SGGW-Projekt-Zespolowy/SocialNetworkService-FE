84218a0e466460ef7d1dfebd47988a77e40b5a53
Tomasz-Budny <s208246@sggw.edu.pl>
2023-06-29 13:02:56 +0200

    Add user-detailed resolver and bind with actual data

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,3 +1,5 @@
+import { PostModel } from "./post.model";
+
 export interface UserModel {
   id: string,
   email: string,
@@ -9,3 +11,20 @@ export interface UserModel {
   profilePictore: string,
 }
 
+export interface UserDetailed {
+  id: number,
+  email: string,
+  lastLogin: Date,
+  registrationTime: Date,
+  firstName: string,
+  lastName: string,
+  dateOfBirth: Date,
+  defree: string,
+  isVerifired: boolean,
+  profilePicture: string,
+  posts: PostModel[],
+  followers: any[],
+  followedByMeUsers: any[],
+  publications: any[],
+  contacts: any[]
+}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
 import { PostPopUpService } from '../services/post-pop-up.service';
 import { ConfirmModalService } from '../services/confirm-modal.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserDetailed } from '../models/user.model';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-user-profile',
@@ -13,12 +16,20 @@ export class UserProfileComponent {
   constructor(
     public popUpService: PostPopUpService,
     protected postService: PostService,
-    public confirmModalService: ConfirmModalService
+    public confirmModalService: ConfirmModalService,
+    private activatedRoute: ActivatedRoute
   ) {}
 
+  userDetailed$: Observable<UserDetailed>;
   selectedTab: string;
 
   selectTab(tab: string): void {
     this.selectedTab = tab;
+  }
+
+  ngOnInit() {
+    this.userDetailed$ = this.activatedRoute.data.pipe(map(data => {
+      return data['userDetailed'];
+    }))
   }
 }
